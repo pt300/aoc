61cdea4663a58a92eb541e093e7d237c3c37132f
P4t <pt300@tlen.pl>
2024-12-01 18:45:32 +0100

    rust: day 1, part 1

---
+++ 2024/rust/day1/src/main.rs
@@ -0,0 +1,23 @@
+use std::io;
+use std::io::BufRead;
+use std::iter::zip;
+
+fn main() {
+    let mut lines = io::stdin().lock().lines();
+    let mut a: Vec<i32> = Vec::new();
+    let mut b: Vec<i32> = Vec::new();
+
+    while let Some(Ok(line)) = lines.next() {
+        if line.len() == 0 { break; }
+        let mut ids = line.split_ascii_whitespace();
+        
+        a.push(ids.next().unwrap().parse().unwrap());
+        b.push(ids.next().unwrap().parse().unwrap());
+    }
+
+    a.sort_unstable();
+    b.sort_unstable();
+
+    let result: i32 = zip(a, b).map(|v| (v.0 - v.1).abs()).sum();
+    println!("Result: {}", result)
+}
